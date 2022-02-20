336a2369b390bad9f78b912077248f62449b8c8d
kyang5 <kyang5@luc.edu>
2022-02-20 16:51:49 -0600

    Implemented methods in User

--- src/com/university/model/use/User.java
+++ src/com/university/model/use/User.java
@@ -1,4 +1,41 @@
 package com.university.model.use;
 
 public class User {
+
+    private String userFirstName;
+    private String userLastName;
+    private int userId;
+    private String userTitle;
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserTitle() {
+        return userTitle;
+    }
+
+    public void setUserTitle(String userTitle) {
+        this.userTitle = userTitle;
+    }
 }
