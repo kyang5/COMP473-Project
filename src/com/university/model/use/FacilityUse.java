9b787d1de96b1f36c45bbd48027fc374a4122391
kyang5 <kyang5@luc.edu>
2022-02-20 16:47:24 -0600

    Implemented methods in FacilityUse

--- src/com/university/model/use/FacilityUse.java
+++ src/com/university/model/use/FacilityUse.java
@@ -1,4 +1,49 @@
 package com.university.model.use;
 
 public class FacilityUse {
+    private boolean inUse;
+    private int capacity;
+    private boolean atCapacity;
+    private int usage;
+    // useStartTime
+    // useEndTime
+
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isAtCapacity() {
+        return atCapacity;
+    }
+
+    public void requestAvailableCapacity() {
+
+    }
+
+    public int getUsage() {
+        return usage;
+    }
+
+    public void isInUseDuringInterval() {
+        //useStartTime, useEndTime
+    }
+
+    public void assignFacilityToUse() {
+
+    }
+
+    public void vacateFacility() {
+
+    }
+
+    // listActualUsage(), delete or move to different class?
+
+    public void calculateUsageRate() {
+
+    }
+
+    public void setInUseDuringInterval() {
+
+    }
 }
