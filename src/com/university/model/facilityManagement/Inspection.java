5323229380a71926c2480656b2d1c9e31b2dfeee
bfujioka <bfujioka@luc.edu>
2022-02-20 12:28:03 -0600

    Drafted Inspection, Inspector, and MaintenanceWorker classes.

--- src/com/university/model/facilityManagement/Inspection.java
+++ src/com/university/model/facilityManagement/Inspection.java
@@ -1,4 +1,42 @@
 package com.university.model.facilityManagement;
-
+import java.util.ArrayList;
+import java.util.List;
+//TODO consider changing inspectionLog to a List or other type
+//TODO check add method.
+//TODO check remove method
+//TODO check list.
 public class Inspection {
+    private String inspectionName;
+    private int inspectionLog;
+    private int inspectionID;
+    private List<Inspector> inspectors = new ArrayList<>();
+
+    public String getInspectionName(){
+        return inspectionName;
+    }
+    public int getInspectionLog(){
+        return inspectionLog;
+    }
+    public int getInspectionID(){
+        return inspectionID;
+    }
+    public void setInspectionName(String inspectionName){
+        this.inspectionName = inspectionName;
+    }
+    public void setInspectionLog(int inspectionLog){
+        this.inspectionLog = inspectionLog;
+    }
+    public void setInspectionID(int inspectionID){
+        this.inspectionID = inspectionID;
+    }
+    public void addInspector(Inspector inspector){
+        inspectors.add(inspector);
+
+    }
+    public void removeInspector(Inspector inspector){
+        inspectors.remove(inspector);
+
+    }
+
+
 }
--- src/com/university/model/facilityManagement/Inspector.java
+++ src/com/university/model/facilityManagement/Inspector.java
@@ -1,4 +1,47 @@
 package com.university.model.facilityManagement;
 
 public class Inspector {
+    //Variables for information about the inspector
+    //TODO Consider giving the Strings unique identifier names
+    //TODO check with whether or not the variables need to be private
+    //TODO check the naming schemas of the parameters
+    private String firstName;
+    private String lastName;
+    private int inspectorID;
+    private String title;
+
+    public String getInspectorFirstName(){
+        return firstName;
+
+    }
+    public String getInspectorLastName(){
+        return lastName;
+
+    }
+    public int getInspectorID(){
+        return inspectorID;
+
+    }
+    public String getInspectorTitle(){
+        return title;
+
+    }
+    public void setInspectorFirstName(String firstName){
+        this.firstName = firstName;
+
+
+    }
+    public void setInspectorLastName(String lastName){
+        this.lastName = lastName;
+
+    }
+    public void setInspectorID(int inspectorID){
+        this.inspectorID = inspectorID;
+
+    }
+    public void setInspectorTitle(String title){
+        this.title = title;
+
+    }
+
 }
