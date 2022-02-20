e0d02bf854c0bb8530f1e3dd4b0c49a19355c63d
kyang5 <kyang5@luc.edu>
2022-02-20 15:44:36 -0600

    Implemented methods in FacilityDetail

--- src/com/university/model/facility/FacilityDetail.java
+++ src/com/university/model/facility/FacilityDetail.java
@@ -1,4 +1,37 @@
 package com.university.model.facility;
 
 public class FacilityDetail {
+    private Integer phoneNumber;
+    private Integer facilityId;
+    private Integer roomNumber;
+
+    public FacilityDetail (Integer phoneNumber, Integer facilityId, Integer roomNumber) {
+        this.phoneNumber = phoneNumber;
+        this.facilityId = facilityId;
+        this.roomNumber = roomNumber;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(Integer facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 }
