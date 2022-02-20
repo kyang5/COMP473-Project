82e15b3d36a8ca82a1ce764c8395577ae365619c
kyang5 <kyang5@luc.edu>
2022-02-20 15:45:42 -0600

    Implemented methods in FacilityManager

--- src/com/university/model/facility/FacilityManager.java
+++ src/com/university/model/facility/FacilityManager.java
@@ -1,4 +1,37 @@
 package com.university.model.facility;
 
 public class FacilityManager {
+    private String managerFirstName;
+    private String managerLastName;
+    private Integer managerId;
+
+    public FacilityManager(String managerFirstName, String managerLastName, Integer managerId) {
+        this.managerFirstName = managerFirstName;
+        this.managerLastName = managerLastName;
+        this.managerId = managerId;
+    }
+
+    public String getManagerFirstName() {
+        return managerFirstName;
+    }
+
+    public void setManagerFirstName(String managerFirstName) {
+        this.managerFirstName = managerFirstName;
+    }
+
+    public String getManagerLastName() {
+        return managerLastName;
+    }
+
+    public void setManagerLastName(String managerLastName) {
+        this.managerLastName = managerLastName;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
 }
