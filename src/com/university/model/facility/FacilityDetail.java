ae414d1975121e7871fc9587a4da0529574f8823
kyang5 <kyang5@luc.edu>
2022-02-21 14:14:23 -0600

    Changed Integer types to int

--- src/com/university/model/facility/FacilityDetail.java
+++ src/com/university/model/facility/FacilityDetail.java
@@ -1,37 +1,37 @@
 package com.university.model.facility;
 
 public class FacilityDetail {
-    private Integer phoneNumber;
-    private Integer facilityId;
-    private Integer roomNumber;
+    private int phoneNumber;
+    private int facilityId;
+    private int roomNumber;
 
-    public FacilityDetail (Integer phoneNumber, Integer facilityId, Integer roomNumber) {
+    public FacilityDetail (int phoneNumber, int facilityId, int roomNumber) {
         this.phoneNumber = phoneNumber;
         this.facilityId = facilityId;
         this.roomNumber = roomNumber;
     }
 
-    public Integer getPhoneNumber() {
+    public int getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(Integer phoneNumber) {
+    public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public Integer getFacilityId() {
+    public int getFacilityId() {
         return facilityId;
     }
 
-    public void setFacilityId(Integer facilityId) {
+    public void setFacilityId(int facilityId) {
         this.facilityId = facilityId;
     }
 
-    public Integer getRoomNumber() {
+    public int getRoomNumber() {
         return roomNumber;
     }
 
-    public void setRoomNumber(Integer roomNumber) {
+    public void setRoomNumber(int roomNumber) {
         this.roomNumber = roomNumber;
     }
 }
