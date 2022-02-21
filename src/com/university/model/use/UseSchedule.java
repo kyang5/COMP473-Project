88ae5e8eaecac4be1f836ef2cdc87e50b945386a
kyang5 <kyang5@luc.edu>
2022-02-21 14:13:17 -0600

    Moved attributes and methods from FacilityUse to UseSchedule

--- src/com/university/model/use/UseSchedule.java
+++ src/com/university/model/use/UseSchedule.java
@@ -1,4 +1,48 @@
 package com.university.model.use;
 
 public class UseSchedule {
+    private boolean inUse;
+    private int capacity;
+    private boolean atCapacity;
+    private int usage;
+    // useStartTime
+    // useEndTime
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isAtCapacity() {
+        return atCapacity;
+    }
+
+    public void requestAvailableCapacity() {
+
+    }
+
+    public int getUsage() {
+        return usage;
+    }
+
+    public void isInUseDuringInterval() {
+        //useStartTime, useEndTime
+    }
+
+    public void assignFacilityToUse() {
+
+    }
+
+    public void vacateFacility() {
+
+    }
+
+    // listActualUsage(), delete or move to different class?
+
+    public void calculateUsageRate() {
+
+    }
+
+    public void setInUseDuringInterval() {
+
+    }
 }
