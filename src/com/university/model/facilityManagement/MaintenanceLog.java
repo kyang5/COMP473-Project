25f23dd9479b197cd83bf0032b42e9edfd1391a0
bfujioka <bfujioka@luc.edu>
2022-02-22 14:04:25 -0600

    Finished drafting the classes for MaintenanceLog, MaintenanceOrder, MaintenanceRequest, and MaintenanceSchedule. Some methods still need to be filled.

--- src/com/university/model/facilityManagement/MaintenanceLog.java
+++ src/com/university/model/facilityManagement/MaintenanceLog.java
@@ -1,4 +1,51 @@
 package com.university.model.facilityManagement;
 
+import com.university.model.facility.FacilityDetail;
+import java.util.*;
+
 public class MaintenanceLog {
+    private double cost;
+    private Date downTime;
+    private List<FacilityDetail> maintenanceList = new ArrayList<FacilityDetail>();
+
+    public double getCost(){
+        return cost;
+    }
+    public Date getDownTime(){
+        //TODO check to see if this needs to be public void
+        return downTime;
+    }
+    public void setCost(int cost){
+        this.cost =cost;
+
+    }
+    public void setDownTime(Date downTime){
+        this.downTime = downTime;
+    }
+    public int calcMaintenanceCostForFacility(){
+        //TODO enter the method for the calculations
+    }
+    public void calcDownTimeForFacility(){
+        //TODO enter the method. Decide whether we need Time or Date as the value
+    }
+    public void listMaintenance(){
+        //TODO decide on the return value and calculations etc.
+    }
+    public void addMaintOrder(){
+        //TODO add values from MaintOrder
+
+    }
+    public void removeMaintWorker(){
+        //TODO add values for MaintWorker
+    }
+    public void addMaintRequest(){
+        //TODO add values for addMaintRequest
+    }
+    public void addMaintSchedule(){
+        //TODO add values for addMaintSchedule
+    }
+    public void addInspection(Inspection addInspection){
+        addInspections.add(addInspection);
+        //Make it a Inspection list?
+    }
 }
--- src/com/university/model/facilityManagement/MaintenanceRequest.java
+++ src/com/university/model/facilityManagement/MaintenanceRequest.java
@@ -1,4 +1,64 @@
 package com.university.model.facilityManagement;
+import java.util.*;
 
 public class MaintenanceRequest {
+    private String requestType;
+    private int requestID;
+    private Date requestDate;
+    private Time requestTime;
+    private int requestorID;
+    private Boolean requestStatus;
+    private String problem;
+
+    public void makeFacilityMaintRequest(){
+
+    }
+    public void scheduleMaintenance(){
+
+    }
+    public int getRequestID(){
+        return requestID;
+    }
+    public Date getRequestDate(){
+        return requestDate;
+    }
+    public Time getRequestTime(){
+        return requestTime;
+    }
+    public Boolean requestStatus(){
+        return requestStatus;
+    }
+    public int getRequestorID(){
+        return requestorID;
+    }
+    public void setRequestType(String requestType){
+        this.requestType = requestType;
+    }
+    public void setRequestID(int requestID){
+        this.requestID = requestID;
+    }
+    public void setRequestDate(Date requestDate){
+        this.requestDate = requestDate;
+    }
+    public void setRequestTime(Time requestTime){
+        this.requestTime = requestTime;
+    }
+    public void setRequestorID(int requestorID){
+        this.requestorID = requestorID;
+    }
+    public void setRequestStatus(Boolean requestStatus){
+        this.requestStatus = requestStatus;
+    }
+    public void listMaintRequest(){
+        //TODO create array list?
+        //TODO Insert method
+    }
+    public void cancelMaintenanceRequest(){
+        //TODO need to include a remove method
+    }
+    public void listFacilityProblems(){
+        //TODO create a list?
+        //TODO return statement
+    }
+
 }
