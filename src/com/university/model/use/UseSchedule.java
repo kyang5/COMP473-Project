55d082adfa11266bf6b9efa1aee57b7cb5e5bda8
kyang5 <kyang5@luc.edu>
2022-02-22 12:26:59 -0600

    Added TODOs

--- src/com/university/model/use/UseSchedule.java
+++ src/com/university/model/use/UseSchedule.java
@@ -1,12 +1,20 @@
 package com.university.model.use;
 
+// TODO: Create method to calculate if reaching capacity (capacity - occupancy)
+// TODO: Make information from FacilityLocation or FacilityDetail available
+// TODO: Add useStartTime, useEndTime, useStartDate, and UseEndDate attributes
+
 public class UseSchedule {
     private boolean inUse;
     private int capacity;
+    private int occupancy;
     private boolean atCapacity;
     private int usage;
-    // useStartTime
-    // useEndTime
+
+
+    public boolean isInUse() {
+        return inUse;
+    }
 
     public int getCapacity() {
         return capacity;
@@ -36,7 +44,7 @@ public class UseSchedule {
 
     }
 
-    // listActualUsage(), delete or move to different class?
+//TODO: Add listActualUsage() method
 
     public void calculateUsageRate() {
 
