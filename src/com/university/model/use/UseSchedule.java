76080800633476ca3ce4361c18658e1287d81672
kyang5 <kyang5@luc.edu>
2022-02-22 15:31:23 -0600

    Added variables, methods and TODOs

--- src/com/university/model/use/UseSchedule.java
+++ src/com/university/model/use/UseSchedule.java
@@ -1,12 +1,12 @@
 package com.university.model.use;
 
-// TODO: Create method to calculate if reaching capacity (capacity - occupancy)
-// TODO: Make information from FacilityLocation or FacilityDetail available
 // TODO: Add useStartTime, useEndTime, useStartDate, and UseEndDate attributes
+// TODO: Consider moving capacity and occupancy to FacilityDetail
 
 import com.university.model.facility.FacilityDetail;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class UseSchedule {
@@ -16,26 +16,44 @@ public class UseSchedule {
     private boolean atCapacity;
     private int usage;
     private List<FacilityDetail> facilityDetails = new ArrayList<>();
+    //private Time useStartTime;
+    //private Time useEndTime;
+    private Date useStartDate;
+    private Date useEndDate;
+    private int useLog;
+    private User user;
 
 
     public boolean isInUse() {
         return inUse;
     }
 
-    public int getCapacity() {
-        return capacity;
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
     }
 
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
+    public int getCapacity() {
+        return capacity;
+    }
+
     public boolean isAtCapacity() {
         return atCapacity;
     }
 
-    public void requestAvailableCapacity() {
+    public int getOccupancy() {
+        return occupancy;
+    }
 
+    public void setOccupancy(int occupancy) {
+        this.occupancy = occupancy;
+    }
+
+    public void requestAvailableCapacity() {
+    // TODO: Calculate capacity - occupancy
     }
 
     public List<FacilityDetail> getFacilityDetails() {
@@ -46,33 +64,61 @@ public class UseSchedule {
         this.facilityDetails = facilityDetails;
     }
 
-    public void addFacilityDetail(FacilityDetail facilityDetail) {
+    public void assignFacilityDetailToUse(FacilityDetail facilityDetail) {
         facilityDetails.add(facilityDetail);
+    }
+
+    public void removeFacilityDetail(FacilityDetail facilityDetail) {
+        facilityDetails.remove(facilityDetail);
     }
 
     public int getUsage() {
         return usage;
     }
 
-    public void assignFacilityToUse() {
+    public void setUsage(int usage) {
+        this.usage = usage;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void addUser(User user) {
+    // TODO: make a list of users?
+    }
+
+    public void removeUser(User user) {
 
     }
 
-    public void vacateFacility() {
-
+    public void listActualUsage() {
+    // TODO: make a list of how rooms in facility are being used?
     }
-
-//TODO: Add listActualUsage() method
 
     public void calculateUsageRate() {
-
+    // TODO: Calculate usage rate
     }
 
+// TODO: Add methods for use times and use dates
+
     public void isInUseDuringInterval() {
-        //useStartTime, useEndTime
+    // TODO: Calculate duration room in facility is being used
     }
 
     public void setInUseDuringInterval() {
+    // TODO: Set to in use during time interval
+    }
 
+    public int getUseLog() {
+        return useLog;
+    }
+
+    public void setUseLog(int useLog) {
+        this.useLog = useLog;
     }
 }
