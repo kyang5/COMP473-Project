18f01c7606bdee06aa74771f24990d77fa3709ec
kyang5 <kyang5@luc.edu>
2022-02-25 13:21:18 -0600

    Added list of facilityLocations, get, set, add and remove methods for listFacilityLocations.

--- src/com/university/model/facility/FacilityManager.java
+++ src/com/university/model/facility/FacilityManager.java
@@ -1,9 +1,13 @@
 package com.university.model.facility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FacilityManager {
     private String managerFirstName;
     private String managerLastName;
     private int managerId;
+    private List<FacilityLocation> listFacilities = new ArrayList<>();
 
     public String getManagerFirstName() {
         return managerFirstName;
@@ -27,5 +31,21 @@ public class FacilityManager {
 
     public void setManagerId(int managerId) {
         this.managerId = managerId;
+    }
+
+    public List<FacilityLocation> getListFacilities() {
+        return listFacilities;
+    }
+
+    public void setListFacilities(List<FacilityLocation> listFacilities) {
+        this.listFacilities = listFacilities;
+    }
+
+    public void addFacilities(FacilityLocation facilityLocation) {
+        listFacilities.add(facilityLocation);
+    }
+
+    public void removeFacilities(FacilityLocation facilityLocation) {
+        listFacilities.remove(facilityLocation);
     }
 }
