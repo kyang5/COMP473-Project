b0fd8e6e3120c4a5a558619fc9d4e47f29c743a8
kyang5 <kyang5@luc.edu>
2022-02-26 16:49:14 -0600

    Added getFacilityLocation()

--- src/com/university/dal/FacilityDAO.java
+++ src/com/university/dal/FacilityDAO.java
@@ -1,4 +1,12 @@
 package com.university.dal;
 
+import com.university.model.facility.FacilityLocation;
+
 public class FacilityDAO {
+
+    public FacilityLocation getFacilityLocation(String failityId) {
+        try{
+
+        }
+    }
 }
