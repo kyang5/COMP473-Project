dd8856d695bfb471ee0faa2a0ab3c241c585ae67
kyang5 <kyang5@luc.edu>
2022-02-28 13:34:01 -0600

    Created add and remove tests

---
+++ src/test/java/FacilityManagerTest.java
@@ -0,0 +1,31 @@
+package test.java;
+
+import com.university.model.facility.FacilityLocation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacilityManagerTest {
+
+    private List<FacilityLocation> location;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addFacilities() {
+    }
+
+    @Test
+    void removeFacilities() {
+    }
+}
