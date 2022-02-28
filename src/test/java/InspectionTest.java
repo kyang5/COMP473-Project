25fe3520b7ef0671e674c440a4d652ffc6fe5a37
kyang5 <kyang5@luc.edu>
2022-02-28 16:19:29 -0600

    Added add/remove inspector from inspectorList tests

--- src/test/java/InspectionTest.java
+++ src/test/java/InspectionTest.java
@@ -1,30 +1,46 @@
 package test.java;
 
+import com.university.model.facilityManagement.Inspector;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InspectionTest {
 
+    private List<Inspector> inspectorList;
+
     @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
+        inspectorList = new ArrayList<>();
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+        inspectorList = null;
     }
 
     @org.junit.jupiter.api.Test
     void addInspector() {
+        Inspector inspector = new Inspector();
+        inspector.setInspectorID(1);
+        inspector.setInspectorFirstName("Dan");
+        inspector.setInspectorLastName("Tan");
+        inspector.setInspectorTitle("Fire Safety Inspector");
+        inspectorList.add(inspector);
+        assertEquals(true, inspectorList.contains(inspector));
     }
 
     @org.junit.jupiter.api.Test
     void removeInspector() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void addInspectorList() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void removeInspectorList() {
+        Inspector inspector = new Inspector();
+        inspector.setInspectorID(1);
+        inspector.setInspectorFirstName("Dan");
+        inspector.setInspectorLastName("Tan");
+        inspector.setInspectorTitle("Fire Safety Inspector");
+        inspectorList.add(inspector);
+        inspectorList.remove(inspector);
+        assertTrue(inspectorList.isEmpty());
     }
 }
