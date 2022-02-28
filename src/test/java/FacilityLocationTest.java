f2727630ce148a36646b10abb9fc56ad9f225215
kyang5 <kyang5@luc.edu>
2022-02-28 13:33:22 -0600

    Created add and remove tests

---
+++ src/test/java/FacilityLocationTest.java
@@ -0,0 +1,97 @@
+package test.java;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.university.model.facility.FacilityLocation;
+import com.university.model.facility.FacilityRoom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FacilityLocationTest {
+
+    private List<FacilityLocation> locationList;
+    private List<FacilityRoom> roomList;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() throws Exception{
+        locationList = new ArrayList<>();
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() throws Exception{
+        locationList = null;
+    }
+
+    @org.junit.jupiter.api.Test
+    void addFacilityLocation() {
+        FacilityLocation facility = new FacilityLocation();
+        facility.setFacilityId(1);
+        facility.setName("Murphy Building");
+        facility.setAddressNumber(123);
+        facility.setStreetName("State Street");
+        facility.setCity("Chicago");
+        facility.setZipcode(123456);
+        locationList.addFacilityLocation(facility); // TODO resolve issue
+        assertEquals(facility, locationList.get(0).intValue()); // TODO what would intValue() be replaced with
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFacilityLocation() {
+        FacilityLocation facility = new FacilityLocation();
+        facility.setFacilityId(1);
+        facility.setName("Murphy Building");
+        facility.setAddressNumber(123);
+        facility.setStreetName("State Street");
+        facility.setCity("Chicago");
+        facility.setZipcode(123456);
+        locationList.addFacilityLocation(facility);
+        locationList.removeFacilityLocation(facility);
+        assertTrue(locationList.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void addFacilityRoom() {
+        FacilityLocation facility = new FacilityLocation();
+        facility.setFacilityId(1);
+        facility.setName("Murphy Building");
+        facility.setAddressNumber(123);
+        facility.setStreetName("State Street");
+        facility.setCity("Chicago");
+        facility.setZipcode(123456);
+
+        FacilityRoom facilityRoom1 = new FacilityRoom();
+        facilityRoom1.setFacilityRoomId(1);
+        facilityRoom1.setPhoneNumber(123-456-7890);
+        facilityRoom1.setCapacity(10);
+        facilityRoom1.setInUse(true);
+        facilityRoom1.setFacilityLocation(facility);
+        roomList.addFacilityRoom(facilityRoom1);
+        assertEquals(facilityRoom1, roomList.get(0).intValue());
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFacilityRoom() {
+        FacilityLocation facility = new FacilityLocation();
+        facility.setFacilityId(1);
+        facility.setName("Murphy Building");
+        facility.setAddressNumber(123);
+        facility.setStreetName("State Street");
+        facility.setCity("Chicago");
+        facility.setZipcode(123456);
+
+        FacilityRoom facilityRoom1 = new FacilityRoom();
+        facilityRoom1.setFacilityRoomId(1);
+        facilityRoom1.setPhoneNumber(123-456-7890);
+        facilityRoom1.setCapacity(10);
+        facilityRoom1.setInUse(true);
+        facilityRoom1.setFacilityLocation(facility);
+        roomList.addFacilityRoom(facilityRoom1);
+        roomList.removeFacilityRoom(facilityRoom1);
+        assertTrue(roomList.isEmpty());
+    }
+}
