1252f2bb9d99b026b283d4cb2f7179decf6cf8f2
kyang5 <kyang5@luc.edu>
2022-02-27 21:22:54 -0600

    Created objects

--- src/com/university/view/FacilityClient.java
+++ src/com/university/view/FacilityClient.java
@@ -1,6 +1,26 @@
 package com.university.view;
 
+import com.university.model.facility.FacilityLocation;
+import com.university.model.facility.FacilityManager;
+import com.university.model.facility.FacilityRoom;
+
 public class FacilityClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        FacilityLocation facilityLocation = new FacilityLocation();
+        facilityLocation.setFacilityId(1);
+        facilityLocation.setName("Murphy Building");
+        facilityLocation.setAddressNumber(123);
+        facilityLocation.setStreetName("State Street");
+        facilityLocation.setCity("Chicago");
+        facilityLocation.setZipcode(123456);
+
+
+        FacilityManager facilityManager = new FacilityManager();
+        facilityLocation.setFacilityManager(facilityManager);
+
+        FacilityRoom facilityRoom1 = new FacilityRoom();
+        facilityRoom1.setFacilityRoomId(1);
+        facilityLocation.addFacilityRoom(facilityRoom1);
     }
 }
