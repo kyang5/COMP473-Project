bf45097e1e07c3cdcf23caab7d02a518140d1596
bfujioka <bfujioka@luc.edu>
2022-02-28 21:15:32 -0600

    Updated MaintenanceLogTest with the inspector tests.

--- src/test/java/MaintenanceLogTest.java
+++ src/test/java/MaintenanceLogTest.java
@@ -1,5 +1,7 @@
 package test.java;
 
+import com.university.model.facility.FacilityLocation;
+import com.university.model.facility.FacilityManager;
 import com.university.model.facility.FacilityRoom;
 import com.university.model.facilityManagement.*;
 
@@ -34,10 +36,83 @@ class MaintenanceLogTest {
 
     @org.junit.jupiter.api.Test
     void addInspection() {
+        FacilityLocation facilityLocation = new FacilityLocation();
+        facilityLocation.setFacilityId(1);
+        facilityLocation.setName("Murphy Building");
+        facilityLocation.setAddressNumber(123);
+        facilityLocation.setStreetName("State Street");
+        facilityLocation.setCity("Chicago");
+        facilityLocation.setZipcode(123456);
+
+        FacilityManager facilityManager = new FacilityManager();
+        facilityManager.setManagerId(1);
+        facilityManager.setManagerFirstName("Bob");
+        facilityManager.setManagerLastName("Doe");
+        facilityLocation.setFacilityManager(facilityManager);
+
+        FacilityRoom facilityRoom1 = new FacilityRoom();
+        facilityRoom1.setFacilityRoomId(1);
+        facilityRoom1.setPhoneNumber(123-456-7890);
+        facilityRoom1.setCapacity(10);
+        facilityRoom1.setInUse(true);
+        facilityRoom1.setFacilityLocation(facilityLocation);
+
+        Inspector inspector = new Inspector();
+        inspector.setInspectorID(1);
+        inspector.setInspectorFirstName("Dan");
+        inspector.setInspectorLastName("Tan");
+        inspector.setInspectorTitle("Fire Safety Inspector");
+
+        Inspection inspection = new Inspection();
+        inspection.setInspectionID(1);
+        inspection.setInspectionName("Fire safety");
+        inspection.setInspector(inspector);
+        inspection.setFacilityRoom(facilityRoom1);
+
+        inspectionList.add(inspection);
+        assertEquals(true, inspectionList.contains(inspection));
     }
+
+
 
     @org.junit.jupiter.api.Test
     void removeInspection() {
+        FacilityLocation facilityLocation = new FacilityLocation();
+        facilityLocation.setFacilityId(1);
+        facilityLocation.setName("Murphy Building");
+        facilityLocation.setAddressNumber(123);
+        facilityLocation.setStreetName("State Street");
+        facilityLocation.setCity("Chicago");
+        facilityLocation.setZipcode(123456);
+
+        FacilityManager facilityManager = new FacilityManager();
+        facilityManager.setManagerId(1);
+        facilityManager.setManagerFirstName("Bob");
+        facilityManager.setManagerLastName("Doe");
+        facilityLocation.setFacilityManager(facilityManager);
+
+        FacilityRoom facilityRoom1 = new FacilityRoom();
+        facilityRoom1.setFacilityRoomId(1);
+        facilityRoom1.setPhoneNumber(123-456-7890);
+        facilityRoom1.setCapacity(10);
+        facilityRoom1.setInUse(true);
+        facilityRoom1.setFacilityLocation(facilityLocation);
+
+        Inspector inspector = new Inspector();
+        inspector.setInspectorID(1);
+        inspector.setInspectorFirstName("Dan");
+        inspector.setInspectorLastName("Tan");
+        inspector.setInspectorTitle("Fire Safety Inspector");
+
+        Inspection inspection = new Inspection();
+        inspection.setInspectionID(1);
+        inspection.setInspectionName("Fire safety");
+        inspection.setInspector(inspector);
+        inspection.setFacilityRoom(facilityRoom1);
+
+        inspectionList.add(inspection);
+        inspectionList.remove(inspection);
+        assertTrue(inspectionList.isEmpty());
     }
 
     @org.junit.jupiter.api.Test
