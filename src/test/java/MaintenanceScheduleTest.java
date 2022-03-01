792fe26834a27b332fbe30616109419a8522bec6
bfujioka <bfujioka@luc.edu>
2022-02-28 20:52:54 -0600

    Updated MaintenanceScheduleTest and UseScheduleTest

--- src/test/java/MaintenanceScheduleTest.java
+++ src/test/java/MaintenanceScheduleTest.java
@@ -1,15 +1,23 @@
 package test.java;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaintenanceScheduleTest {
+    private Date maintenanceStartDate;
+    private Date maintenanceEndDate;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        Date maintenanceStartDate;
+        Date maintenanceEndDate;
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+        Date maintenanceStartDate = null;
+        Date maintenanceEndDate = null;
     }
 
     @org.junit.jupiter.api.Test
--- src/test/java/UseScheduleTest.java
+++ src/test/java/UseScheduleTest.java
@@ -1,15 +1,47 @@
 package test.java;
 
+import com.university.model.facility.FacilityRoom;
+import com.university.model.use.Type;
+import com.university.model.use.User;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UseScheduleTest {
+    private int availableCapacity;
+    private int occupancy;
+    private double usageRate;
+    private Date useStartDate;
+    private Date useEndDate;
+    private List<Type> listActualUsage = new ArrayList<>();
+    private List<User> listUsers = new ArrayList<>();
+    private List<FacilityRoom> facilityRooms = new ArrayList<>();
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        int availableCapacity;
+        int occupancy;
+        double usageRate;
+        Date useStartDate;
+        Date useEndDate;
+        List<Type> listActualUsage = new ArrayList<>();
+        List<User> listUsers = new ArrayList<>();
+        List<FacilityRoom> facilityRooms = new ArrayList<>();
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+        availableCapacity = null;
+        int occupancy;
+        double usageRate;
+        Date useStartDate;
+        Date useEndDate;
+        listActualUsage = null;
+        listUsers = null;
+        facilityRooms = null;
     }
 
     @org.junit.jupiter.api.Test
