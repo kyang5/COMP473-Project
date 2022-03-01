173b60403ac1917df6c1ae06ced22d90e76af319
kyang5 <kyang5@luc.edu>
2022-02-28 21:19:08 -0600

    Updated

--- src/test/java/InspectionTest.java
+++ src/test/java/InspectionTest.java
@@ -1,5 +1,9 @@
 package test.java;
 
+import com.university.model.facility.FacilityLocation;
+import com.university.model.facility.FacilityManager;
+import com.university.model.facility.FacilityRoom;
+import com.university.model.facilityManagement.Inspection;
 import com.university.model.facilityManagement.Inspector;
 
 import java.util.ArrayList;
@@ -12,7 +16,7 @@ class InspectionTest {
     private List<Inspector> inspectorList;
 
     @org.junit.jupiter.api.BeforeEach
-    void setUp() throws Exception{
+    void setUp() throws Exception {
         inspectorList = new ArrayList<>();
     }
 
@@ -22,23 +26,80 @@ class InspectionTest {
     }
 
     @org.junit.jupiter.api.Test
-    void addInspector() {
+    void addInspection() {
+        FacilityLocation facilityLocation = new FacilityLocation();
+        facilityLocation.setFacilityId(1);
+        facilityLocation.setName("Murphy Building");
+        facilityLocation.setAddressNumber(123);
+        facilityLocation.setStreetName("State Street");
+        facilityLocation.setCity("Chicago");
+        facilityLocation.setZipcode(123456);
+
+        FacilityManager facilityManager = new FacilityManager();
+        facilityManager.setManagerId(1);
+        facilityManager.setManagerFirstName("Bob");
+        facilityManager.setManagerLastName("Doe");
+        facilityLocation.setFacilityManager(facilityManager);
+
+        FacilityRoom facilityRoom1 = new FacilityRoom();
+        facilityRoom1.setFacilityRoomId(1);
+        facilityRoom1.setPhoneNumber(123 - 456 - 7890);
+        facilityRoom1.setCapacity(10);
+        facilityRoom1.setInUse(true);
+        facilityRoom1.setFacilityLocation(facilityLocation);
+
         Inspector inspector = new Inspector();
         inspector.setInspectorID(1);
         inspector.setInspectorFirstName("Dan");
         inspector.setInspectorLastName("Tan");
         inspector.setInspectorTitle("Fire Safety Inspector");
+
+        Inspection inspection = new Inspection();
+        inspection.setInspectionID(1);
+        inspection.setInspectionName("Fire safety");
+        inspection.setInspector(inspector);
+        inspection.setFacilityRoom(facilityRoom1);
+
         inspectorList.add(inspector);
         assertEquals(true, inspectorList.contains(inspector));
     }
 
+
     @org.junit.jupiter.api.Test
-    void removeInspector() {
+    void removeInspection() {
+        FacilityLocation facilityLocation = new FacilityLocation();
+        facilityLocation.setFacilityId(1);
+        facilityLocation.setName("Murphy Building");
+        facilityLocation.setAddressNumber(123);
+        facilityLocation.setStreetName("State Street");
+        facilityLocation.setCity("Chicago");
+        facilityLocation.setZipcode(123456);
+
+        FacilityManager facilityManager = new FacilityManager();
+        facilityManager.setManagerId(1);
+        facilityManager.setManagerFirstName("Bob");
+        facilityManager.setManagerLastName("Doe");
+        facilityLocation.setFacilityManager(facilityManager);
+
+        FacilityRoom facilityRoom1 = new FacilityRoom();
+        facilityRoom1.setFacilityRoomId(1);
+        facilityRoom1.setPhoneNumber(123 - 456 - 7890);
+        facilityRoom1.setCapacity(10);
+        facilityRoom1.setInUse(true);
+        facilityRoom1.setFacilityLocation(facilityLocation);
+
         Inspector inspector = new Inspector();
         inspector.setInspectorID(1);
         inspector.setInspectorFirstName("Dan");
         inspector.setInspectorLastName("Tan");
         inspector.setInspectorTitle("Fire Safety Inspector");
+
+        Inspection inspection = new Inspection();
+        inspection.setInspectionID(1);
+        inspection.setInspectionName("Fire safety");
+        inspection.setInspector(inspector);
+        inspection.setFacilityRoom(facilityRoom1);
+
         inspectorList.add(inspector);
         inspectorList.remove(inspector);
         assertTrue(inspectorList.isEmpty());
